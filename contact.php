d4e5c14b4c94cb6f8b941fd95ff5055e2596e117
LaurisDesktop <glaurynasg@gmail.com>
2020-10-16 13:28:40 +0300

    Final_final_push

--- contact.php
+++ contact.php
@@ -17,6 +17,7 @@ $background_num = rand(1, 3);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
     <title>Contact</title>
 </head>
